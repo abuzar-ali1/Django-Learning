16a2cc417c7c62d2e634e03f0efb1fa7bab5265c
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-03-09 20:48:55 +0500

    Createda model and serilizer file

--- app2/admin.py
+++ app2/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Profile , HackathonIdea ,Teacher
+from .models import Profile , HackathonIdea ,Teacher , Student
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(HackathonIdea)
 admin.site.register(Teacher)
-admin.site.register(Teacher)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display =['id' , 'name' , 'roll' , 'city']
